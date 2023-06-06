05c7d8b18fcf8a80812dc9d8824c1d3e9f26b4f5
tr41nwr3ck <27775879+tr41nwr3ck@users.noreply.github.com>
2023-06-06 15:36:12 +0100

    Update main.py
    Removed Mythic

--- C2_Profiles/discord/main.py
+++ C2_Profiles/discord/main.py
@@ -1,6 +1,6 @@
 import mythic_container
 import subprocess
-from discord.mythic.c2_functions.discord import *
+from discord.c2_functions.discord import *
 
 p = subprocess.Popen(["dotnet", "publish", "-c", "Release", "-o", "/Mythic/discord/c2_code/"], cwd="/Mythic/discord/c2_code/src/discord")
 p.wait()
